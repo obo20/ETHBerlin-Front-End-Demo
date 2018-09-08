ac8646b5ff4eaee894ba4efab3cfa0e8c7c3dc9e
Matt Ober <mattober@MacBook-Pro-6.local>
2018-09-08 23:57:35 +0200

    Removed unused code

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { PinataBlue, PinataPink, PinataYellow } from "./theme";
-import ReactSVG from "react-svg";
-import PinataSVG from "./images/pinataSVG.svg";
-import PinataReverseSVG from "./images/PinataReverseSVG.svg";
 import Navigation from './components/navigation'
-import { Button, Icon, Menu, MenuItem, Popover, PopoverInteractionKind, Position } from "@blueprintjs/core";
-
 import { NavigationContext, NavigationProvider } from './contexts';
 import ProviderStats from "./pages/ProviderStats";
 import HomePage from "./pages/homePage";
